c0773a4f7f4c7e2c8fab1a399688057809d89dab
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2022-09-09 19:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 function getItems(){
     db.collection("shopping-web").onSnapshot((snapshot)=>{
         let items=[];
+
+        console.log(snapshot.docs[0].data())
+
+ 
         snapshot.docs.forEach((doc)=>(
             items.push({
                 id:doc.id,
                 ...doc.data()
             })
         ))
-        console.log(items)
+
         generateItems(items);
     })
 }
-
 function generateItems(items){
     let itemsHMTL="";
-    items.forEach((item)=>{
-        itemsHMTL+= `<div class="card1">
-        <div class="Item-img">
-            <img src=${item.image}>
-        </div>
-        <div class="Item-text">
-            <h1>${item.name}</h1>
-        <p class="price">&#8377; ${item.price}</p>
-        <p>${item.description}</p>
-        </div>
-        <div class="add-cart">
-            <button>ADD CART</button>
-        </div>        
-    </div>`    
+    items.forEach((item,index)=>{
+
+        itemsHMTL+= ` 
+        <div class="card1">
+                <div class="Item-img">
+                  <img src = ${item.image}>
+                </div>
+                <div class="Item-text">
+                    <h1 class= "name" id="${index}" > ${item.name} </h1>
+                <p class="price">&#8377; ${item.price}</p>
+                <p class="description">${item.description}</p>
+                </div>
+                <div class="add-cart">
+                <button  type="button" onclick = "addCart()">ADD CART</button>
+                </div>
+            </div>
+                       
+        
+                       
+        `
+        
     })
-document.querySelector(".products").innerHTML=itemsHMTL;   
+document.querySelector(".products").innerHTML=itemsHMTL;
+    
 }
-  
-getItems()
+
+   
+
+
+   
+getItems();
+
+     
+
+     
