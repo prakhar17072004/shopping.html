c9dbda190947ad06946a67d086fa85e5202d985e
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2022-09-23 21:01:37 +0530

    Update card.js

--- card.js
+++ card.js
@@ -3,6 +3,7 @@ function getItems() {
 	db.collection('cart-web').onSnapshot((snapshot) => {
 		snapshot.docs.forEach((doc) =>
 			items.push({
+				id:doc.id,
 				...doc.data()
 			})
 		);
@@ -11,19 +12,34 @@ function getItems() {
 }
 
 function generateItems(items) {
+	console.log(items[0]);
 	let itemsHMTL = '';
 	items.forEach((item) => {
 		itemsHMTL += `
-       <div class="cart-box">
-                <img src = ${item.image} class="cart-img">
+               <div class="cart-box">
+                   <img src = ${item.image} class="cart-img">
                     <div class="Item-detail">
                          <div class="cart-product-title">${item.name}</div>
                          <div class="cart-price">$ ${item.price}</div>
                     </div>
-            </div>   
+					<button type="button" class=" cart-remove" onclick = removeItemFromCart("${item.id}") > REMOVE </button>
+               </div>   
         `;
 	});
 	document.querySelector('.cart-container').innerHTML = itemsHMTL;
 }
 
 getItems();
+
+
+
+function removeItemFromCart(id){
+db.collection('cart-web').doc(`${id}`).delete().then(() => {
+    console.log("Document successfully deleted!");
+	getItems();
+}).catch((error) => {
+    console.error("Error removing document: ", error);
+});
+}
+	
+	
